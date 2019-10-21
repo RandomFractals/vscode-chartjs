ddb6bf5f964764f42fd8525136546433f5b90a6b
Taras Novak <taras.novak@randomfractals.com>
2019-10-21 10:22:28 -0500

    #1 added ext. info, commands and images

--- out/extension.js
+++ out/extension.js
@@ -12,7 +12,13 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('chart.view', () => {
+    let disposable = vscode.commands.registerCommand('chart.preview', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello Charts!');
+    });
+    context.subscriptions.push(disposable);
+    disposable = vscode.commands.registerCommand('chart.samples', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello Charts!');
--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chart.view', () => {
+	let disposable = vscode.commands.registerCommand('chart.preview', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello Charts!');
 	});
-
 	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('chart.samples', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello Charts!');
+	});
+	context.subscriptions.push(disposable);
+
 }
 
 // this method is called when your extension is deactivated
